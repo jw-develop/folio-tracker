8042ba9681b182b6705fa08f0195908e0e3169ad
James White <james.white@my.wheaton.edu>
2020-03-26 23:43:43 -0400

    List of stocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
+import { CSVLink } from 'react-csv';
+import { CsvToHtmlTable } from "react-csv-to-table";
 import './App.css';
+
+// Documentation for API at https://intrinio.com/
+const stocks: string[] = [
+  "AAPL",
+  "CHK",
+  "CVS",
+  "EOG",
+  "EVBG",
+  "MCD",
+  "MSFT",
+  "NRG",
+  "NUE",
+  "QDEL",
+  "TDOC",
+  "XOM"
+]
+
+class FolioTable extends React.Component <{}, { data: string[][] }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      data: stocks.map(e => [e]),
+    }
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={() => {
+          let grid = this.state.data;
+          grid[0].push("babs")
+          this.setState({
+            data: grid,
+          })
+        }}>
+          Add Val To Data
+        </button>
+        <button><CSVLink data={this.state.data}filename={"folio-export.csv"}>Download CSV</CSVLink></button>
+        <CsvToHtmlTable
+          id="mainTable"
+          data={simplifyCSV(this.state.data)}
+          csvDelimiter=","
+          tableClassName="table"
+        />
+      </div>
+    )
+  }
+}
+
+function simplifyCSV(arrays: string[][]) {
+  let toReturn = "";
+  arrays.forEach(array => {
+    array.forEach(x => {
+      toReturn += x + ",";
+    })
+    toReturn += "\n";
+  });
+  return toReturn;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FolioTable></FolioTable>
       </header>
     </div>
   );
---
+++ src/stocks.ts
@@ -0,0 +1,16 @@
+
+
+export const stocks: string[] = [
+    "AAPL",
+    "CHK",
+    "CVS",
+    "EOG",
+    "EVBG",
+    "MCD",
+    "MSFT",
+    "NRG",
+    "NUE",
+    "QDEL",
+    "TDOC",
+    "XOM"
+]
